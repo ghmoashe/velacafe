c15ecc66d3448d70804bdc2e13149a216dbfe8d7
Ghazanfar <gmaosher@gmail.com>
2026-03-28 11:21:57 +0100

    vela1057

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('src/i18nData.ts')) {
+            return 'i18n'
+          }
+          if (id.includes('node_modules/@supabase/')) {
+            return 'supabase'
+          }
+        },
+      },
+    },
+  },
 })
